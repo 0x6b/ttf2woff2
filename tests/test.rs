db1fc5386d505085f7f568d0ea2b6e651264824b
kaoru <k@warpnine.io>
2024-04-18 17:18:46 +0900

    refactor: update signature

--- tests/test.rs
+++ tests/test.rs
@@ -12,7 +12,7 @@ async fn test() -> Result<()> {
     let input = root.join("NotoSansJP-Medium.ttf");
     let output = root.join("NotoSansJP-Medium.woff2");
 
-    let converter = Converter::from(input, output.clone(), BrotliQuality::default()).await?;
+    let converter = Converter::from(input, Some(output.clone()), BrotliQuality::default()).await?;
     let (input_size, output_size) = converter.to_woff2().await?;
 
     // pre-calculated SHA-256 hash and output file size using `woff2_compress` command from
